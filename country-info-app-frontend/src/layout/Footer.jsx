ba7e7105aedfddab6e18989f3e9d2d37c6c861e6
matheusmartinsviana <matheusmviana@outlook.com.br>
2024-09-10 17:12:43 -0300

    feat: add my markdown on footer :)

--- country-info-app-frontend/src/layout/Footer.jsx
+++ country-info-app-frontend/src/layout/Footer.jsx
@@ -1,7 +1,9 @@
+import { Link } from 'react-router-dom';
 
 export default function Footer() {
     return (
-        <footer>
+        <footer className='flex justify-center items-center mt-10'>
+            <p title='Developed by Matheus Martins Viana'>&copy;2024 Developed by <Link to={'https://www.linkedin.com/in/matheusmartinsviana/'} target='_blank'>Matheus Martins Viana</Link></p>
         </footer>
     )
 }

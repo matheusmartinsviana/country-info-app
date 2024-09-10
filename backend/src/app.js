d4c29ffe7e678dce2617c8e16f985acc301d51e3
matheusmviana@outlook.com.br <matheusmviana@outlook.com.br>
2024-09-10 11:38:21 -0300

    fix: get country capital city

--- backend/src/app.js
+++ backend/src/app.js
@@ -35,21 +35,68 @@ app.get('/api/v1/CountryInfo/:countryCode', async (req, res) => {
         });
         const data = await response.json();
 
+        const populationData = await getCountryPopulationData(data.commonName)
         const populationNumber = await getCountryPopulation(data.commonName);
         const countryFlag = await getCountryFlag(countryCode);
-        console.log(data)
+
         return res.json({
             name: data.commonName,
             countryCode: data.countryCode,
             borders: data.borders,
             population: populationNumber,
-            flag: countryFlag
+            flag: countryFlag,
+            populationData: populationData
         });
     } catch (error) {
         console.error('Error fetching country info:', error);
         return res.status(500).json({ error: 'Error fetching country info.' });
     }
 });
+
+const getCountryCity = async (countryName) => {
+    try {
+        const response = await fetch('https://countriesnow.space/api/v0.1/countries/capital', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ country: countryName })
+        })
+        const result = await response.json()
+
+        if (result.error == true) {
+            return data.msg
+        }
+
+        const capital = result.data.capital
+        return capital
+    } catch (error) {
+
+    }
+}
+
+const getCountryPopulationData = async (countryName) => {
+    try {
+        const capital = await getCountryCity('Armenia')
+        const response = await fetch('https://countriesnow.space/api/v0.1/countries/population/cities', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ city: capital })
+        });
+        const result = await response.json()
+        if (result.error == true) {
+            return result.msg
+        }
+        const populationData = result.data.populationCounts
+
+        return populationData
+    } catch (error) {
+        console.error('Error fetching country population:', error);
+        throw new Error('Error fetching country population');
+    }
+}
 
 const getCountryPopulation = async (city) => {
     try {
@@ -65,9 +112,7 @@ const getCountryPopulation = async (city) => {
             return data.msg
         }
         const populationData = data.data.populationCounts
-
         const latestPopulation = populationData[populationData.length - 1]
-
         return latestPopulation ? latestPopulation.value : 'Population data not available'
     } catch (error) {
         console.error('Error fetching country population:', error);
@@ -86,7 +131,12 @@ const getCountryFlag = async (countryCode) => {
         });
 
         const data = await response.json();
-        return data.data.flag;;
+
+        if (data.error == true) {
+            return data.msg
+        }
+
+        return data.data.flag;
     } catch (error) {
         console.error('Error fetching country flag:', error);
         throw new Error('Error fetching country flag');
